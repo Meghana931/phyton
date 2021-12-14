89deffa8656e4ca115f7bb8dd901e68406e9de80
Meghana931 <86868270+Meghana931@users.noreply.github.com>
2021-12-14 17:15:19 +0530

    Create algorithm.py

---
+++ algorithm.py
@@ -0,0 +1,12 @@
+1) 10,1,8,3,6,5,4,7,x,y
+find general solution of x and y? write an algorithm for the same.
+ans) the above series follows alternate series (i.e) even and odd numbers
+     x is a variable which follows decreasing even number series
+     x = 4-2 = 2   x=2
+     y is a variable which follows increasing odd number series
+     y = 7+2 = 9   y=9
+     ALOGIRTHM:
+     Step1: Start
+     Step2: get x and y
+     Step3: x = 4-2=2 and y = 7+2=9
+     Step4: stop
