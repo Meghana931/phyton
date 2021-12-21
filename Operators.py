88f35a9ee1c42ac81b16bbe86948a5674730120f
Meghana931 <86868270+Meghana931@users.noreply.github.com>
2021-12-21 11:27:29 +0530

    Create Operators.py

---
+++ Operators.py
@@ -0,0 +1,54 @@
+ #LOGICAL OPERATORS
+#In logical operators are used on conditional statements(either True or
+ False). 
+ #They perform Logical AND ,Logical OR and Logical NOT Operations.                                                        
+ # Logical and operator
+a = 100
+b = 200
+if a > 0 and b > 0:
+    print("the numbers are greater than 0")
+    # Logical or operator
+a = 250
+b = 1
+if a > 0 or b > 0:
+    print("either of numbers are greater than 0")
+ #logical not operator
+x = 5
+print(not(x > 3 and x < 10))
+#returns false because not is used to reverse the result                                                      
+#COMPARISION OPERATORS  
+comparison operators are also called relational operators as they find the relation between the operands 
+(greater than, equal,less than,greater than equal to,less than equal to, not equal to)
+and generate a boolean value in terms of True and False.
+a = 4
+b = 8
+print(a == b)
+#returns false
+a = 4
+b = 8
+print(a != b)
+#returns true
+a = 4
+b = 8
+print(a > b)
+#returns false
+a = 4
+b = 8
+print(a < b)
+#returns true
+a = 10
+b = 10
+print(a >= b)
+#returns true 
+a = 10
+b = 10
+print(a >= b)
+#returns true 
+
+
+
+
+
+
+
+
